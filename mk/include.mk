6e702e940b717b558594a863792ae1fbbaa9bec7
Sergey Egorov <sergey.egorov@teleste.com>
2020-02-03 11:02:02 +0200

    Add basic support for multiarch build

---
+++ mk/include.mk
@@ -0,0 +1,6 @@
+# Copyright 2017 Intel Corporation. 
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+GOARCH ?= amd64
+include $(PATH_TO_MK)/include_$(GOARCH).mk
